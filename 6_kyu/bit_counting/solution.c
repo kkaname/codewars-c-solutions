062ad35ba401ffcbd83fbbeae1c92123acbdd321
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-16 18:52:07 +0530

    Fix: improved the code styling format

--- 6_kyu/bit_counting/solution.c
+++ 6_kyu/bit_counting/solution.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-size_t countBits(unsigned value)
-{
+size_t countBits(unsigned value) {
   if (value == 0)
     return 0;
 
   int num_of_ones = 0;
-  while (value != 0){
-    if (value % 2 == 1)
+  while (value != 0) {
+    if (value % 2 == 1) {
       ++num_of_ones;
+    }
     value = value / 2;
   }
 
@@ -19,6 +19,7 @@ int main(void) {
     unsigned int num;
     printf("Enter a non-negative integer: ");
     scanf("%u", &num);
+
     printf("Number of bits equal to one: %zu\n", countBits(num));
 
     return 0;
