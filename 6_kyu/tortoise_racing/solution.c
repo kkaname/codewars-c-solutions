e235bfcb36043c6f13cac616241aba5a2309fef4
Ayush Hegde <ayushhegde2005@gmail.com>
2026-01-01 08:17:22 +0530

    fix: changed the logic and added comments

--- 6_kyu/tortoise_racing/solution.c
+++ 6_kyu/tortoise_racing/solution.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int power(int num, int n);
-float round_float(float num, int n);
 int *race(int v1, int v2, int g);
 
 int main(void) {
     int v1, v2, g;
-    v1 = 720;
-    v2 = 850;
-    g = 70;
+    v1 = 720;   //speed of tortoise A
+    v2 = 850;   //speed of tortoise B
+    g = 70; //distance gap between them initially
 
     int *ptr = race(v1, v2, g);
     printf("For v1 = %d, v2 = %d, gap = %d: T = ", v1, v2, g);
-
     for (int i = 0; i < 3; i++) {
         printf("%d ", ptr[i]);
     }
@@ -35,40 +32,21 @@ int main(void) {
     return 0;
 }
 
-int power(int num, int n) {
-    int product = 1;
-    for (int i = 0; i < n; i++) {
-        product = product * num;
-    }
-    return product;
-}
-
-float round_float(float num, int n) {
-    num = (int)(num * power(10, 4));
-    return (num / power(10, 4));
-}
-
 int *race(int v1, int v2, int g) {
-    int *return_ptr = malloc(3 * sizeof(int));
-    for (int i = 0; i < 3; i++) {
-      return_ptr[i] = -1;
-    }
+    int *result = malloc(3 * sizeof(int));
 
     if (v1 >= v2) {
-      return return_ptr;
+        for (int i = 0; i < 3; i++) {
+          result[i] = -1;
+        }
+        return result;
     }
 
-    float time = (float)g / (float)(v2 - v1);
-    for (int i = 0; i < 3; i++) {
-      if (i == 0) {
-        return_ptr[i] = time;
-        continue;
-      }
+    int total_seconds = (g * 3600) / (v2 - v1);
 
-      time = (float)time - (int)time;
-      time = round_float(time, 4) * 60;
-      return_ptr[i] = time;
-    }
+    result[0] = total_seconds / 3600;
+    result[1] = (total_seconds % 3600) / 60;
+    result[2] = total_seconds % 60;
 
-	return return_ptr;
+	return result;
 }
