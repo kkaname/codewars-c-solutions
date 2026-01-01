b65e3c39143fcd9d5a4d3d592e460eef38bc5da5
Ayush Hegde <ayushhegde2005@gmail.com>
2026-01-01 08:17:22 +0530

    Add solution for Convert String to Camel(6kyu, C)

---
+++ 6_kyu/convert_string_to_camel/solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void to_camel_case(const char *text, char *camel) {
+    int i, j;
+    j = 0;
+    for (i = 0; text[i] != 0; i++) {
+        if (text[i] != '-' && text[i] != '_') {
+            camel[j++] = text[i];
+        }
+        else {
+            ++i;
+            if (text[i] >= 97 && text[i] <= 122) {
+                camel[j++] = text[i] - 32;
+            }
+            else {
+                camel[j++] = text[i];
+            }
+        }
+    }
+}
+
+int main(void) {
+   const char text[] = "the-stealth-warrior";
+   char *camel = (char *)malloc(strlen(text) + 1);
+   to_camel_case(text, camel);
+   printf("%s\n", camel);
+   free(camel);
+
+   const char text1[] = "The_Stealth_Warriors";
+   char *camel1 = (char *)malloc(strlen(text1) + 1);
+   to_camel_case(text1, camel1);
+   printf("%s\n", camel1);
+   free(camel1);
+
+   return 0;
+}
